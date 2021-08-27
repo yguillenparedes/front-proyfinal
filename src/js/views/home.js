6635d013d15d3a7ef0b07a97f3f70464eddff886
Angel Restifo <angelrestifo36@gmail.com>
2021-08-27 19:12:45 -0400

    avance de formulario de servicio

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,19 +1,102 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
 import "../../styles/home.scss";
 
 export const Home = () => (
-	<div>
-		<div className="text-center mt-5">
-			<h1>Formulario de Registro de Servicio</h1>
+	<div className="container text-center mt-5">
+		<div ClassName="row">
+			<div ClassName="col">
+				<h1>Formulario de Registro de Servicio</h1>
+			</div>
 		</div>
-		<div className="column">
-			<a>
-				<p>Nombre de empresa</p>
-			</a>
+		<div className="forms row col-12">
+			<div className="col-6">
+				<a>Nombre de empresa</a>
+			</div>
+			<div className="col-6">
+				<TextField label="Size" id="outlined-size-normal" defaultValue="Normal" variant="outlined" />
+			</div>
 		</div>
-		<div className="column">
-			<TextField id="filled-basic" label="Filled" variant="filled" />
+
+		<div className="forms row col-12">
+			<div className="col-6">
+				<a>Categoria de servicio</a>
+			</div>
+			<div className="col-6">
+				<TextField
+					id="outlined-select-currency"
+					select
+					label="Select"
+					helperText="Please select your currency"
+					variant="outlined">
+					<MenuItem />
+				</TextField>
+			</div>
+		</div>
+
+		<div className="forms row col-12">
+			<div className="col-6">
+				<a>Descripcion de servicio</a>
+			</div>
+			<div className="col-6">
+				<TextField label="Size" id="outlined-size-normal" defaultValue="Normal" variant="outlined" />
+			</div>
+		</div>
+
+		<div className="forms row col-12">
+			<div className="col-6">
+				<a>Tipo de servicio</a>
+			</div>
+			<div className="col-6">
+				<TextField
+					id="outlined-select-currency"
+					select
+					label="Select"
+					helperText="Please select your currency"
+					variant="outlined">
+					<MenuItem />
+				</TextField>
+			</div>
+		</div>
+
+		<div className="forms row col-12">
+			<div className="col-6">
+				<a>Estado</a>
+			</div>
+			<div className="col-6">
+				<TextField
+					id="outlined-select-currency"
+					select
+					label="Select"
+					helperText="Please select your currency"
+					variant="outlined">
+					<MenuItem />
+				</TextField>
+			</div>
+		</div>
+
+		<div className="forms row col-12">
+			<div className="col-6">
+				<a>Ciudad</a>
+			</div>
+			<div className="col-6">
+				<TextField
+					id="outlined-select-currency"
+					select
+					label="Select"
+					helperText="Please select your currency"
+					variant="outlined">
+					<MenuItem />
+				</TextField>
+			</div>
+		</div>
+
+		<div className="register">
+			<Button variant="outlined" color="secondary">
+				Secondary
+			</Button>
 		</div>
 	</div>
 );
