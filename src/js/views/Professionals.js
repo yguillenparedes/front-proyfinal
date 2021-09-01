a6bc3f410658bc5c40bb86d3865c75937b728d4a
Jonathan Diaz <jonathandc0708@gmail.com>
2021-09-01 17:13:14 -0400

    otro cambio faltante

--- src/js/views/Professionals.js
+++ src/js/views/Professionals.js
@@ -1,70 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import { Card } from "react-bootstrap";
-import { Button } from "react-bootstrap";
-import "../../img/profesionals.jpg";
-import "../../styles/profesionals.scss";
-
-export const Home = () => (
-	<div className="landing container-fluid">
-		<div className="profesionals text-center col-12 d-flex">
-			<div className="col-6 d-flex justify-content-center">
-				<Card style={{ width: "32rem", height: "27rem" }}>
-					<Card.Img variant="top" src="profesionals.jpg" style={{ width: "32rem", height: "27rem" }} />
-				</Card>
-			</div>
-
-			<div className="col-3">
-				<div className="probutton">
-					<h2>Lorem Ipsum</h2>
-					<p>Lorem Ipsum Lorem Ipsum Lorem Ipsum</p>
-				</div>
-			</div>
-			<div className="col-3">
-				<div className="probutton">
-					<Button variant="info" size="lg">
-						Contacto
-					</Button>
-				</div>
-			</div>
-		</div>
-		<div className="row">
-			<div className="col-sm-3">
-				<div className="card">
-					<div className="card-body">
-						<h5 className="card-title">Review</h5>
-						<p className="card-text">Descripción</p>
-					</div>
-				</div>
-			</div>
-			<div className="col-sm-3">
-				<div className="card">
-					<div className="card-body">
-						<h5 className="card-title">Review</h5>
-						<p className="card-text">Descripción</p>
-					</div>
-				</div>
-			</div>
-			<div className="col-sm-3">
-				<div className="card">
-					<div className="card-body">
-						<h5 className="card-title">Review</h5>
-						<p className="card-text">Descripción</p>
-					</div>
-				</div>
-			</div>
-			<div className="col-sm-3">
-				<div className="card">
-					<div className="card-body">
-						<h5 className="card-title">Review</h5>
-						<p className="card-text">Descripción</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-);
-=======
 import React, { useContext } from "react";
 import Card from "../component/card";
 import { Context } from "../store/appContext";
@@ -93,4 +26,3 @@ export const Professionals = props => {
 		</div>
 	);
 };
->>>>>>> develop
